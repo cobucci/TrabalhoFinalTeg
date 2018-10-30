0383152bba9615fa50d9341acfec16c6187cbc6d
Lucas Cobucci <37388214+cobucci@users.noreply.github.com>
2018-10-30 00:48:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,10 @@ int main(){
 
     int V = 6;
 	Grafo grafo(V);
-    double y;
-    
-    y = (double)rand()/RAND_MAX*1.000-0.000;
-    pair <double, double> coordenada (5, y);
-    grafo.adicionarAresta(0, coordenada);
-    /*
     vector <pair <double, double> > coord;
     gerandoCoord(coord, V);
-
-    gerandoLigacaoEPesoAresta(grafo, V, coord);
+    grafo.gerandoLigacaoEPesoAresta(V, coord);
     grafo.printGrafo(V, coord);
-    */
-    printf()
-	return 0;
+        
+    return 0;
 }
