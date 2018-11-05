5defbc5e79e658609a576d1984b353498b086e09
Lucas Cobucci <37388214+cobucci@users.noreply.github.com>
2018-11-04 23:29:03 -0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 
 int main(){
 
-    int V = 6;
-    Grafo grafo(V);
+    int V = 5;
+	Grafo grafo(V);
     vector <pair <double, double> > coord;
     gerandoCoord(coord, V);
     grafo.gerandoLigacaoEPesoAresta(V, coord);
     grafo.printGrafo(V, coord);
-        
+    grafo.DFS(coord);
+    grafo.DFSModificado(coord);
+    //grafo.BFSAleatorio();
+    
     return 0;
 }
